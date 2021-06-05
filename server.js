4fa0b31722cb46db3436e1d5bd46536d1ea154c4
Ryoma Kobayashi <77302472+ryokoba689@users.noreply.github.com>
2021-06-05 15:07:27 -0700

    Add files via upload
    I had issues pushing the repo online. Git was not letting me push my data up with incorrect remote repo or it didnt exist. I tried git push -u master, origin, branch, making a remote repo. None of which worked for me and will show a more commited repo when able to push

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
